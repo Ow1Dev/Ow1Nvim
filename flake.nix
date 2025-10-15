bb5daa29cf99c090a87118e93a6a0b3c12aae066
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-10-15 17:10:01 +0200

    feat: change so i link with homemanger

--- flake.nix
+++ flake.nix
@@ -72,23 +72,9 @@
             cp -r ${./lua} $out/share/nvim/lua
             cp -r ${./lsp} $out/share/nvim/lsp
             
-            # Create wrapper script for installed version
+            # Simple wrapper - Home Manager handles the config symlinking
             cat > $out/bin/nvim <<EOF
             #!/bin/sh
-            export XDG_CONFIG_HOME="\''${XDG_CONFIG_HOME:-\$HOME/.config}"
-            mkdir -p "\$XDG_CONFIG_HOME/nvim-flake"
-            
-            # Symlink our config if not already present
-            if [ ! -e "\$XDG_CONFIG_HOME/nvim-flake/init.lua" ]; then
-              ln -sf $out/share/nvim/init.lua "\$XDG_CONFIG_HOME/nvim-flake/init.lua"
-            fi
-            if [ ! -e "\$XDG_CONFIG_HOME/nvim-flake/lua" ]; then
-              ln -sf $out/share/nvim/lua "\$XDG_CONFIG_HOME/nvim-flake/lua"
-            fi
-            if [ ! -e "\$XDG_CONFIG_HOME/nvim-flake/lsp" ]; then
-              ln -sf $out/share/nvim/lsp "\$XDG_CONFIG_HOME/nvim-flake/lsp"
-            fi
-            
             export NVIM_APPNAME="nvim-flake"
             exec ${myNeovim}/bin/nvim "\$@"
             EOF
@@ -112,6 +98,7 @@
       homeManagerModules.default = { config, lib, pkgs, ... }: 
         let
           system = pkgs.stdenv.hostPlatform.system;
+          flakePkgs = self.packages.${system};
         in {
           options.programs.neovim-flake = {
             enable = lib.mkEnableOption "Neovim flake configuration";
@@ -119,6 +106,12 @@
 
           config = lib.mkIf config.programs.neovim-flake.enable {
             home.packages = [ self.packages.${system}.installed ];
+            xdg.configFile = {
+              "nvim-flake/init.lua".source = flakePkgs.installed + "/share/nvim/init.lua";
+              "nvim-flake/.luarc.json".source = flakePkgs.installed + "/share/nvim/.luarc.json";
+              "nvim-flake/lua".source = flakePkgs.installed + "/share/nvim/lua";
+              "nvim-flake/lsp".source = flakePkgs.installed + "/share/nvim/lsp";
+            };
           };
         };
     };
