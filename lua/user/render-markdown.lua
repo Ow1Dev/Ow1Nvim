38fb0f19292d9e4f25fb12caa49e27876cab26f8
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-10-20 18:29:41 +0200

    feat: adding render-markdown

---
+++ lua/user/render-markdown.lua
@@ -0,0 +1,7 @@
+local M = {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    opts = {},
+};
+
+return M;
