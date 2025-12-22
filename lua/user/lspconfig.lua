34c28008e1cbab504d8233fdc78c5dee05d7140b
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-12-22 11:18:28 +0100

    fix: adding cr to lsp

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -9,10 +9,10 @@ local M = {
 local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true }
   local keymap = vim.api.nvim_buf_set_keymap
-  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()", opts)
-  keymap(bufnr, "n", "gd", "<cmd>vim.lsp.buf.definition()", opts)
-  keymap(bufnr, "n", "gI", "<cmd>vim.lsp.buf.implementation()", opts)
-  keymap(bufnr, "n", "gr", "<cmd>vim.lsp.buf.references()", opts)
+  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+  keymap(bufnr, "n", "gd", "<cmd>vim.lsp.buf.definition()<cr>", opts)
+  keymap(bufnr, "n", "gI", "<cmd>vim.lsp.buf.implementation()<cr>", opts)
+  keymap(bufnr, "n", "gr", "<cmd>vim.lsp.buf.references()<cr>", opts)
   keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 end
 
