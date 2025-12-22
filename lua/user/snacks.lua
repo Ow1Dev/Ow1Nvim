b417066a3756be19535eb6e1305a7fc1dd3efe03
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-12-22 10:25:03 +0100

    fix: turn of git in explorer again

--- lua/user/snacks.lua
+++ lua/user/snacks.lua
@@ -6,21 +6,28 @@ local M = {
   priority = 1000,
   lazy = false,
   opts = {
-      explorer = {
-        enabled = true,
-        git_status = false,
-        git_untracked = false,
+    explorer = {
+      enabled = true,
+    },
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = {
+          git_status = false,
+          git_untracked = false,
+        },
       },
-      picker = { enabled = true },
+    },
   },
 }
 
 function M.config()
-  local wk = require "which-key"
-  wk.add {
-    { "<C-p>", "<cmd>lua require 'snacks'.picker.files()<cr>", desc = "Open file picker" },
-    { "<leader>e", "<cmd>lua require 'snacks'.explorer.open()<cr>", desc = "Open explorer" },
-  }
+  local wk = require("which-key")
+  wk.add({
+    { "<C-p>", "<cmd>lua require('snacks').picker.files()<cr>", desc = "Open file picker" },
+    { "<leader>e", "<cmd>lua require('snacks').explorer.open()<cr>", desc = "Open explorer" },
+  })
 end
 
 return M
+
